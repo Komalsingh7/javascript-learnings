772cb4a9eaf0e7b9345c55f1bdf8fcd9c1204bfa
Komalsingh7 <162954877+Komalsingh7@users.noreply.github.com>
2024-09-29 13:07:02 +0000

    changed

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -19,3 +19,39 @@ let isLoggedIn = false
 console.log(typeof age);
 console.log(typeof undefined) ; // undefined
 console.log(typeof null); // object
+
+
+// more info on data types
+
+// Primitive -> call by value
+/* 7 types : String , Number , Boolean , null , undefined , Symbol,
+             BigInt  
+*/
+
+
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isloggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherid = Symbol('123');
+console.log(id === anotherid);
+
+const bigNumber =  3456786945646795456346n;
+
+// Reference (Non Primitive)
+// Array , Objects , Functions 
+
+const heroes = ["shaktimaan" , "naagraj" , "doga"];
+let myObj = {
+    name : "hitesh",
+    age: 22 , 
+}
+
+const myFunction = function(){
+      console.log("Hello World");
+}
