b719d41e4b458e5730431e0b38acb4b18444f4cc
Komalsingh7 <162954877+Komalsingh7@users.noreply.github.com>
2024-09-29 15:55:41 +0000

    changes

---
+++ 01_basics/4_strings.js
@@ -0,0 +1,19 @@
+const name = "komal"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+const gameName = new String('hiteshhc');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
